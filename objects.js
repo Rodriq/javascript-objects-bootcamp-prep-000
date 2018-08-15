d0f9e5970458144681c4889992e36e347c837217
Fogang Rodriq <fogangrodriq@gmail.com>
2018-08-15 21:19:15 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,17 @@
 var playlist = {Asheck: 'You too big'}
 
 function updatePlaylist(playlist, artistName, songTitle){
+<<<<<<< HEAD
   playlist[artistName] = songTitle;
   return playlist;
 }
 function removeFromPlaylist(playlist, artistName){
   delete playlist[artistName];
   return playlist
+=======
+  playlist.artistName = 'songTitle';
+}
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.artistName;
+>>>>>>> 06ca2c6f9a7a51e44069e961e5f12c7a0426186c
 }
